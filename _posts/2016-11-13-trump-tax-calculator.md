9c2e1a999b8bb72e928da0dacf0c87d6997460dc
Michael Goldstein <mjgolds2@illinois.edu>
2016-11-13 21:26:12 -0600

    trump tax calculator

---
+++ _posts/2016-11-13-trump-tax-calculator.md
@@ -0,0 +1,52 @@
+---
+layout: post
+title:  "Trump Tax Calculator"
+date:   2016-11-13 21:25:00 -0500
+---
+
+Like it or not, Donald Trump is going to be our next president. One thing 
+Trump promised all along was tax simplification and cuts for the middle
+class. Earlier today, [the good folks at NPR outlined the details of Donald
+Trump's tax plan](http://www.npr.org/2016/11/13/501739277/who-benefits-from-donald-trumps-tax-plan){:target="_blank"}.
+
+My first response of course was to find out how it might affect me. After
+doing the math, I was surprised to learn that I'll likely be paying about
+the same amount of taxes under Trump's plan. Basically, Trump wants to
+reduce the number of tax brackets from seven down to three. The top bracket
+will be taxed at 33% versus 40%. Under Trump's plan, most people will 
+likely end up with an effective tax rate that's a percent or two lower,
+but as NPR points out, those who actually will benefit the most are top
+earners.
+
+I created a handy calculator for you to see how your tax rates could be
+affected. Simply select your filing status and input your total taxable
+income (don't forget to add your bonus). It'll do all the rest of the work
+for you! 
+
+<link href="/css/bootstrap.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/trump.js"></script>
+<div style="border: 1px solid gray; border-radius: 18px; padding: 10px;">
+<h3 style="margin: 0px;">Michael's Tax Comparison Calculator</h3>
+<hr/>
+<div class="btn-group" data-toggle="buttons" style="margin: 10px 0px 10px 0px;">
+    <label class="btn btn-primary active">
+        <input type="radio" name="filing" onchange="calcTaxes()" value="1" autocomplete="off" checked> Single
+    </label>
+    <label class="btn btn-primary">
+        <input type="radio" name="filing" onchange="calcTaxes()" value="2" autocomplete="off"> Married Filing Jointly
+    </label>
+    <label class="btn btn-primary">
+        <input type="radio" name="filing" onchange="calcTaxes()" value="3" autocomplete="off"> Head of Household
+    </label>
+</div>
+<div class="input-group">
+    <span class="input-group-addon">$</span>
+    <input type="number" id="income" onkeyup="calcTaxes()" style="width: 95%;" class="form-control" min="0" step="0.01" placeholder="total taxable income">
+</div>
+<h4 style="margin: 10px 0px 10px 0px;">Right now, you're taxed at about <span id="rate1" style="color: #0000ff;">0.0%</span> and pay about 
+<span id="amt1" style="color: #0000ff;">$0</span>.</h4>
+<h4 style="margin: 10px 0px 5px 0px;">Under Trump's plan, you'd be taxed at about <span id="rate2" style="color: #0000ff;">0.0%</span> and pay about 
+<span id="amt2" style="color: #0000ff;">$0</span>.</h4>
+</div>
---
+++ js/trump.js
@@ -0,0 +1,84 @@
+function calcTaxes() {
+    var filing = parseInt($("input[name='filing']:checked").val());
+    var income = parseFloat($("#income").val());
+    amt1 = calcOldAmt(filing, income);
+    rate1 = (amt1 / income) * 100;
+    amt2 = calcNewAmt(filing, income);
+    rate2 = (amt2 / income) * 100;
+    if (Number.isNaN(amt1)) {
+        amt1 = 0;
+        rate1 = 0;
+    }
+    if (Number.isNaN(amt2)) {
+        amt2 = 0;
+        rate2 = 0;
+    }
+    $("#rate1").html(rate1.toFixed(1) + "%");
+    $("#amt1").html(amt1.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }));
+    $("#rate2").html(rate2.toFixed(1) + "%");
+    $("#amt2").html(amt2.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }));
+}
+
+function calcOldAmt(f, inc) {
+    var tab = [
+        [0.10,  0.15,  0.25,   0.28,   0.33,   0.35,   0.40],     // tax rates
+        [9275,  37650, 91150,  190150, 413350, 415050, Infinity], // upper bounds for single
+        [18550, 75300, 151900, 231450, 413350, 466950, Infinity], // upper bounds for married
+        [13250, 50400, 130150, 210800, 413350, 441000, Infinity]  // upper bounds for HoH
+    ];
+    var tot = 0.0;
+    for (var i = 0; i < 7; i++) {
+        // if above this tax bracket
+        // then amount - last bracket max * rate gets added
+        if (inc > tab[f][i]) {
+            if (i == 0) {
+                tot += tab[f][i] * tab[0][i];
+            } else {
+                tot += (tab[f][i] - tab[f][i-1]) * tab[0][i];
+            }
+        } else {
+            // if we are in tax bracket
+            // add contribution of income - last bracket max * rate
+            // then return
+            if (i == 0) {
+                tot += inc * tab[0][i];
+            } else {
+                tot += (inc - tab[f][i-1]) * tab[0][i];
+            }
+            return tot;
+        }
+    }
+    return 0;
+}
+
+function calcNewAmt(f, inc) {
+    var tab = [
+        [0.12,  0.25,   0.33],     // tax rates
+        [37500, 112500, Infinity], // upper bounds for single
+        [75000, 225000, Infinity], // upper bounds for married
+        [37500, 112500, Infinity]  // upper bounds for HoH
+    ];
+    var tot = 0.0;
+    for (var i = 0; i < 3; i++) {
+        // if above this tax bracket
+        // then amount - last bracket max * rate gets added
+        if (inc > tab[f][i]) {
+            if (i == 0) {
+                tot += tab[f][i] * tab[0][i];
+            } else {
+                tot += (tab[f][i] - tab[f][i-1]) * tab[0][i];
+            }
+        } else {
+            // if we are in tax bracket
+            // add contribution of income - last bracket max * rate
+            // then return
+            if (i == 0) {
+                tot += inc * tab[0][i];
+            } else {
+                tot += (inc - tab[f][i-1]) * tab[0][i];
+            }
+            return tot;
+        }
+    }
+    return 0;
+}
